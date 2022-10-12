56aa6877bd7cd4143083b646e7047468f58810be
willor777 <wcorwig777@gmail.com>
2022-10-12 17:39:12 -0400

    Minor Upgrades

--- src/main/kotlin/com/das/webcrawlertools/WebCrawlerTools.kt
+++ src/main/kotlin/com/das/webcrawlertools/WebCrawlerTools.kt
@@ -5,6 +5,8 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.async
 import okhttp3.OkHttpClient
 import okhttp3.Request
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
 import java.util.logging.Logger
 import kotlin.coroutines.coroutineContext
 
@@ -35,7 +37,7 @@ object WebCrawlerTools{
 
 
     /**
-     * Attempts to retrieve a webpage using a GET request on the url
+     * Attempts to retrieve a webpage using a GET request on the url. Uses a Random User-Agent each time.
      */
     fun getWebpage(url: String): String? {
 
@@ -60,29 +62,10 @@ object WebCrawlerTools{
         return resp.body?.string()
     }
 
-    fun getRandomUserAgent(): String {
-        val userAgents = listOf(
-            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)" +
-                    " Chrome/58.0.3029.110 Safari/537.36",
-            "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:53.0) Gecko/20100101 Firefox/53.0",
-
-            "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.0; Trident/5.0; Trident/5.0)",
-
-            "Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; Trident/6.0; MDDCJS)",
-
-            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)" +
-                    " Chrome/51.0.2704.79 Safari/537.36 Edge/14.14393",
-
-            "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1)"
-        )
-
-        return userAgents[(0..5).random()]
-    }
-
-
 
     /**
-     * Attempts to retrieve webpage using async GET request on url
+     * Attempts to retrieve webpage using async GET request on url. Won't waste system resources while waiting for
+     * web page.
      */
     suspend fun getWebpageAsync(url: String): String? {
 
@@ -111,6 +94,50 @@ object WebCrawlerTools{
         }
 
         return resp.body?.string()
+    }
+
+
+    /**
+     * Randomly Selects a User-Agent from one of six.
+     */
+    fun getRandomUserAgent(): String {
+        val userAgents = listOf(
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)" +
+                    " Chrome/58.0.3029.110 Safari/537.36",
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:53.0) Gecko/20100101 Firefox/53.0",
+
+            "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.0; Trident/5.0; Trident/5.0)",
+
+            "Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; Trident/6.0; MDDCJS)",
+
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)" +
+                    " Chrome/51.0.2704.79 Safari/537.36 Edge/14.14393",
+
+            "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1)"
+        )
+
+        return userAgents[(0..5).random()]
+    }
+
+
+    /**
+     * Retrieves the webpage with given url using random User-Agent, parses it into a Jsoup Document if possible.
+     */
+    fun parseUrl(url: String): Document?{
+        val webPage = getWebpage(url) ?: return null
+
+        return Jsoup.parse(webPage)
+    }
+
+
+    /**
+     * Retrieves the webpage with given url using random User-Agent, parses it into a Jsoup Document if possible.
+     * Does it Asynchronously, so CPU cycles are not wasted.
+     */
+    suspend fun parseUrlAsync(url: String): Document?{
+        val webPage = getWebpageAsync(url) ?: return null
+
+        return Jsoup.parse(webPage)
     }
 
 
